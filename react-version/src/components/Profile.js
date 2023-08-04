1e097fd50cb65f54b05e9b7072741e68f14a441c
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-08-04 19:06:40 -0400

    Set up tweeter-react project

---
+++ react-version/src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h1>I am the profile component</h1>
+    </div>
+  );
+}
+
+export default Profile;
---
+++ react-version/src/components/TweetList.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function TweetList() {
+  return (<h1>I am the tweet list component</h1>);
+}
+export default TweetList;
