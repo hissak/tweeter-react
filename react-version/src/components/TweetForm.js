9b546d18d4caf10db4d65fa19cc7441590800e60
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-08-04 23:14:44 -0400

    Completes work for W06D05

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 function TweetForm() {
+  const tweetPlaceHolder = "What are you humming about?"
+  const preventSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
-    <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+    < section className="newtweet" >
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={preventSubmit}>
+        <textarea className="form__textarea" name="text" placeholder={tweetPlaceHolder}></textarea>
         <input type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
       </form>
